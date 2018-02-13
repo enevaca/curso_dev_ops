dd2bf1d76a79835b3d3bd134eac6667258aab21b
enevaca <noel.vm.2501@gmail.com>
2018-02-12 23:30:24 -0400

    Add html change 6

--- deployments/qa.sh
+++ deployments/qa.sh
@@ -1,5 +1,5 @@
 sudo npm install -g now
 echo "deploying..."
-URL=$(now --docker --public -t NOW_TOKEN)
+URL=$(now --docker --public -t $NOW_TOKEN)
 echo "running acceptance on $URL"
 curl --silent -L $URL
